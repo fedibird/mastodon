9b4ff049b4963bb06acedeaa2fc68c3f8c514090
noellabo <noel.yoshiba@gmail.com>
2026-09-06 08:09:54 +0900

    Fix replies to circle post (bug that added unnecessary mentions)

--- app/services/process_mentions_service.rb
+++ app/services/process_mentions_service.rb
@@ -56,10 +56,10 @@ class ProcessMentionsService < BaseService
       # need to send our reply to the remote author's inbox for distribution
 
       status.thread.mentions.includes(:account).find_each do |mention|
-        status.mentions.create(silent: true, account: mention.account) unless status.account_id == mention.account_id && mentioned_account_ids.include?(mention.account.id)
+        status.mentions.create(silent: true, account: mention.account) unless status.account_id == mention.account_id || mentioned_account_ids.include?(mention.account.id)
       end
 
-      status.mentions.create(silent: true, account: status.thread.account) unless status.account_id == status.thread.account_id && mentioned_account_ids.include?(status.thread.account.id)
+      status.mentions.create(silent: true, account: status.thread.account) unless status.account_id == status.thread.account_id || mentioned_account_ids.include?(status.thread.account.id)
     end
 
     status.save!
