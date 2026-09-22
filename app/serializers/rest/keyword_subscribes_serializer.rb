9ed2ac27a861507722fafa83b412c9a1ca996e88
Takeshi Umeda <noel.yoshiba@gmail.com>
2026-09-22 14:22:20 +0900

    Fix Keyword Subscribe serializer attributes

--- app/serializers/rest/keyword_subscribes_serializer.rb
+++ app/serializers/rest/keyword_subscribes_serializer.rb
@@ -1,7 +1,7 @@
- # frozen_string_literal: true
+# frozen_string_literal: true
 
 class REST::KeywordSubscribesSerializer < ActiveModel::Serializer
-  attributes :id, :name, :keyword, :exclude_keyword, :ignorecase, :regexp, :ignore_block, :disabled, :exclude_home
+  attributes :id, :name, :keyword, :exclude_keyword, :ignorecase, :regexp, :ignore_block, :disabled
 
   def id
     object.id.to_s
